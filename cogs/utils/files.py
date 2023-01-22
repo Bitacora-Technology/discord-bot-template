e23fe6aa84eff692b1646d0c5d6b1b1d52674cd4
galind <guillem.yt1@gmail.com>
2023-01-22 14:05:37 +0100

    File updated to read new cog structure

--- cogs/utils/files.py
+++ cogs/utils/files.py
@@ -2,12 +2,11 @@ import os
 
 folder_path = os.getcwd()
 
-ignored_extensions = ['utils']
-
 
 def get_initial_extensions():
     extensions_list = []
     for extension in os.listdir(f'{folder_path}/cogs/'):
-        if extension not in ignored_extensions:
-            extensions_list.append(f'cogs.{extension}')
+        if extension.endswith('.py'):
+            name = extension[:-3]
+            extensions_list.append(f'cogs.{name}')
     return extensions_list
